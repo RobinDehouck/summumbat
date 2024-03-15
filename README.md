8d272d2b03c994f2d646ce5a02f1415937667ffa
RobinD <robin.dehouck@live.fr>
2024-03-15 14:28:45 +0100

    Prepare for Heroku deployment

---
+++ README.md
@@ -0,0 +1,1 @@
+ReadMe file for Summum Bat.
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Send the user to index.html if they don't specify a file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
