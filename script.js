47fbca29508ea50ed331514d8f111f8c32a696e4
RobinD <robin.dehouck@live.fr>
2024-03-15 12:54:00 +0100

    fixing missing closing tag and adding icons on landing that move

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+AOS.init();
 var scrollpos = window.scrollY;
 var header = document.getElementById("header");
 var navcontent = document.getElementById("nav-content");
@@ -115,4 +116,72 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-AOS.init();
+
+// Create a GSAP timeline with paused state to control the start manually
+const tl = gsap.timeline({ paused: true, onComplete: () => restartAnimation() });
+
+// Function to simulate a click effect on the "+" icon
+function clickPlusEffect() {
+  tl.to(".icon-plus", {
+    scale: 0.8, // Simulate click by scaling down
+    duration: 0.2,
+    yoyo: true, // Go back to original scale
+    repeat: 1, // Repeat the scale down and up once
+    ease: "power1.inOut",
+  });
+}
+
+// Function to animate the icons in
+function animateIconsIn() {
+  clickPlusEffect();
+  tl.from(".solar-panel", { duration: 0.5, y: -50, opacity: 0, ease: "back.out(1.7)" }, "+=0.1");
+  clickPlusEffect();
+  tl.from(".renovation", { duration: 0.5, x: 50, opacity: 0, ease: "back.out(1.7)" }, "+=0.1");
+  clickPlusEffect();
+  tl.from(".ballon-eau-chaude", { duration: 0.5, x: -50, opacity: 0, ease: "back.out(1.7)" }, "+=0.1");
+  clickPlusEffect();
+  tl.from(".heat-pump", { duration: 0.5, x: 50, opacity: 0, ease: "back.out(1.7)" }, "+=0.1");
+  clickPlusEffect();
+  tl.from(".poele", { duration: 0.5, x: -50, opacity: 0, ease: "back.out(1.7)" }, "+=0.1");
+}
+
+// Function to animate icons and the "+" button out
+function animateOut() {
+  tl.to(".icon-plus, .solar-panel, .renovation, .ballon-eau-chaude, .heat-pump, .poele", {
+    opacity: 0,
+    duration: 0.5,
+    ease: "back.in(1.7)",
+    stagger: {
+      amount: 0.5,
+      from: "end"
+    }
+  });
+}
+
+// Function to restart the animation from the beginning
+function restartAnimation() {
+  // Set all icons and the "+" button to full opacity before restart
+  gsap.set(".icon-plus, .solar-panel, .renovation, .ballon-eau-chaude, .heat-pump, .poele", { opacity: 1 });
+  
+  // Reset the timeline to start
+  tl.restart();
+}
+
+// Initial animation setup
+animateIconsIn();
+
+// Wait 5 seconds after the last icon animation to start fading out
+tl.to({}, { duration: 5 }, "+=0.1");
+animateOut();
+
+// Start the animation
+tl.play();
+
+document.addEventListener("scroll", function() {
+  document.querySelectorAll('.image-container').forEach(function(el) {
+    var speed = 0.1; // Ajustez ce taux pour changer la vitesse de l'effet parallaxe
+    var windowScroll = window.pageYOffset; // Obtient la position actuelle du défilement
+    el.style.backgroundPositionY = (windowScroll * speed) + "px";
+  });
+});
+
