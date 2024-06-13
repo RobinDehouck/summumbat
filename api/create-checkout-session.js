c5978576a8e98042227f8e09efb31e3dcb0f9f94
Robin Dehouck <robin.dehouck@skema.edu>
2024-06-13 18:40:41 +0200

    moving server.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
+    console.log('Request received:', req.body);
+
     const { priceId } = req.body;
 
     try {
@@ -11,12 +13,15 @@ module.exports = async (req, res) => {
                 quantity: 1,
             }],
             mode: 'payment',
-            success_url: `https://www.summumbat.fr/`,
-            cancel_url: `https://www.summumbat.fr/`,
+            success_url: `${process.env.VERCEL_URL}/success`,
+            cancel_url: `${process.env.VERCEL_URL}/canceled`,
         });
+
+        console.log('Session created:', session);
 
         res.status(200).json({ id: session.id });
     } catch (error) {
+        console.error('Error creating session:', error);
         res.status(500).json({ error: error.message });
     }
 };
